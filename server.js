bcea59bd72c2350c6159adb02b0255c8c6a09629
hazkeel <hazkeel27@gmail.com>
2023-08-14 03:49:12 -0400

    port issue fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { logMethod } = require('./middleware/logMethod'); // middlewware
 const api = require('./routes/index');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
