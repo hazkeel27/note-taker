81582cacabe10b329fb5748de363d62e572a64ae
hazkeel <hazkeel27@gmail.com>
2023-08-17 02:16:30 -0400

    added gitignore file and frontend js file renderActiveNote functions if statement corrected

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -53,7 +53,7 @@ const deleteNote = (id) =>
 const renderActiveNote = () => {
   hide(saveNoteBtn);
 
-  if (activeNote.id) {
+  if (activeNote && activeNote.id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
